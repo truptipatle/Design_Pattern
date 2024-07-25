ebdfa87a05f72a8857374fcc0644d1e017ff085d
Trupti Patle <90976297+truptipatle@users.noreply.github.com>
2024-07-25 13:02:06 +0530

    commit the code

---
+++ Singleton/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public sealed class Singleton
+    {
+        private Singleton()
+        {
+
+        }
+
+       private  static Singleton Instance;
+
+        public static Singleton GetInstance()
+        {
+            if(Instance == null)
+            {
+                Instance = new Singleton();
+            }
+            return Instance;
+        }
+
+
+        //public void SomebusinessLogic()
+        //{
+
+        //}
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+
+                //Singleton s1 = Singleton.GetInstance();
+
+                Singleton s1 = Singleton.GetInstance();
+                Singleton s2 = Singleton.GetInstance();
+
+                if (s1 == s2)
+                {
+                    Console.WriteLine("singleton Works, both variables contains same instance");
+                }
+                else
+                {
+                    Console.WriteLine("singleton Failed, both vairables contains diffrent insatnce");
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
